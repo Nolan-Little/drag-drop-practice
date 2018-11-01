3330e19db47d27f29fd299e960256446b9a65fec
Nolan-Little <little.nolan@gmail.com>
2018-11-01 11:45:14 -0500

    complete exercise

---
+++ drag.js
@@ -0,0 +1,39 @@
+const DragDropManager = Object.create(null, {
+  init: {
+    value: () => {
+      const stages = document.querySelectorAll(".stage")
+
+      stages.forEach(stage => {
+        // Gain reference of item being dragged
+        stage.ondragstart = e => {
+          e.dataTransfer.setData("text", e.target.classList)
+        }
+      })
+
+
+      const targets = document.querySelectorAll(".target")
+
+      targets.forEach(target => {
+        // Dragover not supported by default. Turn that off.
+        target.ondragover = e => e.preventDefault()
+
+        target.ondrop = e => {
+          // Enabled dropping on targets
+          e.preventDefault()
+
+          // Determine what's being dropped
+          const data = e.dataTransfer.getData("text")
+
+          // Append card to target component as child
+          // TODO: This should only happen if the target has no children nodes
+          if(e.target.childNodes.length === 0 || e.target.classList[1] === "origin"){
+         console.log(e.target.classList)
+          e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`))
+          }
+        }
+      })
+    }
+  }
+})
+
+DragDropManager.init()
